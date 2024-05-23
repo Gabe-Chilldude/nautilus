dbc1eef8d1d850e2cf7aab79f2bc68eaef236ac7
Gabriel Fernandes <mutequinoken09@gmail.com>
2024-05-23 20:44:04 -0300

    Parte parts/bannerWithForeground finalizada

--- parts/bannerWithForeground.php
+++ parts/bannerWithForeground.php
@@ -4,21 +4,20 @@
 
    <div class="ctaAndText">
 
-        <div class="banner_text_container">
-            <p class="banner_text text">
-                leve sua empresa para a nova realidade digital
-            </p>
+        <p class="banner_text text">
+            leve sua empresa para a nova realidade digital
+        </p>
 
-            <h1 class="banner_title text">
-                A sua persona virtual com alta performance
-            </h1>
+        <h1 class="banner_title text">
+            A sua persona virtual com alta performance
+        </h1>
 
-            <p class="banner_text text">
-                Para marcas inovadoras: Brand Persona PYXYS + Ilustraria 3D é o primeiro combo do Brasil
-                de construção de avatares 3D, com tecnologia de ponta em computação gráfica, combinada
-                à mais completa Estratégia de Content, Growth e Performance Data-driven
-            </p>
-        </div>
+        <p class="banner_text text">
+            Para marcas inovadoras: Brand Persona PYXYS + Ilustraria 3D é o primeiro combo do Brasil
+            de construção de avatares 3D, com tecnologia de ponta em computação gráfica, combinada
+            à mais completa Estratégia de Content, Growth e Performance Data-driven
+        </p>
+
 
         <button class="banner_cta">Quero saber mais</button>
 
@@ -26,7 +25,7 @@
             <p class="banner_sponsors banner_text text">Um Produto:</p>
 
             <img src="<?= get_template_directory_uri();?>/assets/images/pyxys-logo-4x.png" alt="" class="sponsor_img">
-            <img src="" alt="" class="sponsor_img">
+            <img src="<?=get_template_directory_uri()?>/assets/images/logo-ilustraria3d-branco.png" alt="" class="sponsor_img">
         </div>
 
    </div>
